48be5b298747bdb2014b36f6ab81ea8af135e50c
Moise Uwimana <moiseuwimana04@gmail.com>
2023-09-28 11:14:10 +0200

    Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import Meme from "./components/Meme"
 
 export default function (){
   return (
     <div>
       <Header />
+      <Meme />
     </div>
   )
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,11 @@
+export default function Meme() {
+  return (
+    <main>
+      <form className="form">
+        <input className="form--input" type="text" placeholder="Top text" />
+        <input className="form--input" type="text" placeholder="Bottom text"/>
+        <button className="form--button">Get a new meme image </button>
+      </form>
+    </main>
+  )
+}
