aec011e29b40230ee30846fb5186402abfffd597
Moise Uwimana <moiseuwimana04@gmail.com>
2023-09-28 15:45:32 +0200

    Get random meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,22 @@
+import memesData from "../memesData"
+
 export default function Meme() {
+  function getMemeImage(){
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url)
+  }
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input className="form--input" type="text" placeholder="Top text" />
         <input className="form--input" type="text" placeholder="Bottom text"/>
-        <button className="form--button">Get a new meme image </button>
-      </form>
+        <button 
+        className="form--button"
+        onClick={getMemeImage}
+        >Get a new meme image </button>
+      </div>
     </main>
   )
 }
